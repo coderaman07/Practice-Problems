79bfb9256680312706214f5fc76ec6fa5301b2df
Coderaman07 <alexmercerr07@gmail.com>
2020-09-05 23:06:16 +0530

    Program to add Two Numbers in C#

---
+++ C#/Sum of Two Numbers.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace C_
+{
+    class Question2
+    {
+        static void MainQuestion2(String[] args){
+            Console.WriteLine("First Number : 10");
+            Console.WriteLine("Second Number : 10");
+            int sum = 10+10;
+            Console.WriteLine("The Sum if Both Number is : " + sum);
+        }
+    }
+}
