bfa6222af6aec9ffc215b3d77017e546a0a17572
Coderaman07 <alexmercerr07@gmail.com>
2020-09-05 23:05:47 +0530

    Program to Display Name and Hello in Diffrent Lines

---
+++ C#/Displaying Name and Hello in Diffrent Lines.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_
+{
+    class Question1
+    {
+        static void MainQuestion1(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.Write("Name of the Developer : Aman Ojha");
+        }
+    }
+}
