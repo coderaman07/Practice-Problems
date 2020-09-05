f68fe07262900c72fd5ae2d924d69eebfc2b3875
Coderaman07 <alexmercerr07@gmail.com>
2020-09-05 23:17:44 +0530

    Program to Print Name and Hello in Diffrent Lines

---
+++ Java/src/PrintingNameandHelloinDiffrentLines.java
@@ -0,0 +1,6 @@
+public class PrintingNameandHelloinDiffrentLines {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Aman Ojha");
+    }
+}
