ca8f5d75a69f71d4261de14c943340726d6f95d8
Coderaman07 <alexmercerr07@gmail.com>
2020-09-05 23:19:27 +0530

    Program to Print the Current Page of the Browser

---
+++ JavaScript/Print Contents of the Screen.js
@@ -0,0 +1,7 @@
+// Grabbing the Element with getElement by ID in JavaScript
+document.getElementById('body').innerHTML = '<button onclick="Printing()">Click Here</button>';
+
+// Creating a Function which will make Everything to Get in Printing Function 
+function Printing(){
+    window.print();
+}
