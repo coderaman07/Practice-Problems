92392361afcecb66376efd8303a95e629a1d7a11
Coderaman07 <alexmercerr07@gmail.com>
2020-09-05 23:15:11 +0530

    Getting the GCC compiler information through C program

---
+++ C/gettingTheCurrentVersionOfGCCcompiler.c
@@ -0,0 +1,9 @@
+// Including the Nessesary Files for Developement
+#include<stdio.h> 
+
+// MainProgram which will be Executed
+int main(int argc, char** argv) {
+
+    // Desplaying the Version with Frmatted String 
+    printf("We are using : "__VERSION__);
+}
