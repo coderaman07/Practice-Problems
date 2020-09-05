3fc16a1582e3ba16f46099c20320f82379037b0c
Coderaman07 <alexmercerr07@gmail.com>
2020-09-05 23:18:09 +0530

    Program to Display Sum of two Numbers

---
+++ Java/src/SumofTwoNumbers.java
@@ -0,0 +1,8 @@
+public class SumofTwoNumbers {
+    public static void main(String[] args){
+        System.out.println("First Number : 10");
+        System.out.println("Second Number : 10");
+        int sum = 10 + 10;
+        System.out.println("The Sum of Both Numbers are : "+sum);
+    }
+}
