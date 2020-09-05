7a28b0c20062065c93b1c92b1876ac27f5811ebd
Coderaman07 <alexmercerr07@gmail.com>
2020-09-05 23:07:49 +0530

    Program to add two Numbers

---
+++ C++/SumOfTwoNumbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+    int firstNumber = 10;
+    int SecondNumber = 10;
+    std::cout<<"First Number : "<<firstNumber;
+    std::cout<<"\nSecond Number : "<<SecondNumber;
+    int sum = firstNumber + SecondNumber;
+    std::cout<<"\nThe Sum off "<<firstNumber<<" and "<<SecondNumber<<" is "<< sum;
+    return 0;
+}
