de30f0ce07b63904629e07901cde8d844800fd49
Coderaman07 <alexmercerr07@gmail.com>
2020-09-06 17:13:26 +0530

    Printing the Letter F and C using #

---
+++ C/PrintingFandCusing#.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void PrintF(){
+    printf(" #####\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" ####\n");
+    printf(" #\n");
+    printf(" #\n");
+}
+
+void PrintC(){
+    printf("    ######\n");
+    printf("  ##      ##\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf("  ##      ##\n");
+    printf("    ######\n");
+}
+
+int main(){
+    PrintC();
+    PrintF();
+}
