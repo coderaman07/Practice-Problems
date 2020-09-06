0fdc7bd4d40cf11131aa2b34ff9e9bb84b666fca
Coderaman07 <alexmercerr07@gmail.com>
2020-09-06 17:14:14 +0530

    Program to Divide two Numbers and Print on Console

---
+++ Java/src/DivideTwoNumbersAndPrintOnScreen.java
@@ -0,0 +1,8 @@
+public class DivideTwoNumbersAndPrintOnScreen {
+    public static void main(String[] args){
+        System.out.println("First Number : 25");
+        System.out.println("Second Number : 5");
+        int Quotient = 25 / 5;
+        System.out.println("The Quotient of 25 and 5 is : " + Quotient);
+    }
+}
