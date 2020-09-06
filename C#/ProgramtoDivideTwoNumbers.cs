a0dce978ef38374ee891eca46073944182a1d2eb
Coderaman07 <alexmercerr07@gmail.com>
2020-09-06 17:10:32 +0530

    Program to Divide Two Numbers

---
+++ C#/ProgramtoDivideTwoNumbers.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace C_
+{
+    class Program
+    {
+        public static void MainQuestion3(String[] args){
+            int firstNumber = 25;
+            int SecondNumber = 5;
+            Console.WriteLine("First Number : " + firstNumber);
+            Console.WriteLine("Second Number : " + SecondNumber);
+            int Quotient = firstNumber / SecondNumber;
+            Console.WriteLine("The Quotient of " + firstNumber + " and " + SecondNumber + " is : " + Quotient);
+        }
+    }
+}
